a31ed51096f406bac9c8fcf30b8750031af2e04c
huangjx <trustfor@sina.cn>
2019-06-25 13:31:49 +0800

    init log

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,13 +1,12 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
+    public static int count;
 
     static {
         System.out.println(1);
     }
 
-    int age = initAge();
     String name;
 
     {
@@ -17,9 +16,12 @@ public class Cat {
     {
         System.out.println(3);
     }
+    int age = initAge();
+
 
     public int initAge() {
         System.out.println(4);
+        count = initStaticCount();
         return 0;
     }
 
@@ -30,12 +32,12 @@ public class Cat {
 
     public Cat(String name) {
         this(0, name);
-        System.out.println(6);
+        System.out.println(7);
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        System.out.println(7);
+        System.out.println(6);
     }
 }
