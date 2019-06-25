6ce3077143750934bfa89109f0de9134ca30d186
Mu Mu <mumuloveruby@gmail.com>
2019-06-25 17:23:11 +0800

    reorder initializers (#15)
    reorder initializers (#15)

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,14 +1,12 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
+    String name;
+    public static int count;
 
     static {
         System.out.println(1);
     }
-
-    int age = initAge();
-    String name;
 
     {
         System.out.println(2);
@@ -17,9 +15,11 @@ public class Cat {
     {
         System.out.println(3);
     }
+    int age = initAge();
 
     public int initAge() {
         System.out.println(4);
+        count = initStaticCount();
         return 0;
     }
 
@@ -30,12 +30,12 @@ public class Cat {
 
     public Cat(String name) {
         this(0, name);
-        System.out.println(6);
+        System.out.println(7);
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        System.out.println(7);
+        System.out.println(6);
     }
 }
