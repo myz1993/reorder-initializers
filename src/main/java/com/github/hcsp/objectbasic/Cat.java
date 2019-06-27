fa62e4c1542132102713eb999eafdd61f139657d
Lingfeng <2354833@qq.com>
2019-06-27 13:13:57 +0800

    虽然过了test，但不确定做的对不对

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,41 +1,45 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
+
+    int age = initAge();
+    String name;
+    public int count = initStaticCount();
 
     static {
         System.out.println(1);
     }
-
-    int age = initAge();
-    String name;
-
-    {
+    static {
         System.out.println(2);
     }
-
-    {
+    static {
         System.out.println(3);
     }
 
-    public int initAge() {
+    public  int initAge() {
         System.out.println(4);
         return 0;
     }
 
-    public static int initStaticCount() {
+
+
+
+    public  int initStaticCount() {
         System.out.println(5);
         return 0;
     }
 
+
     public Cat(String name) {
+
         this(0, name);
-        System.out.println(6);
+
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
+        System.out.println(6);
         System.out.println(7);
     }
 }
