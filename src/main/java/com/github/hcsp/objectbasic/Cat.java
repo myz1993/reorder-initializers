923738a248f163ada3d9b8a79f05e48fcbc54643
Procarihana <48359478+Procarihana@users.noreply.github.com>
2019-07-16 23:40:29 +0800

    对象的初始化顺序 (#29)
     对象的初始化顺序 (#29)

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,20 +1,21 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
+    int age = initAge();
+    int count = initStaticCount();
 
     static {
         System.out.println(1);
     }
 
-    int age = initAge();
+
     String name;
 
-    {
+    static {
         System.out.println(2);
     }
 
-    {
+    static {
         System.out.println(3);
     }
 
@@ -23,19 +24,20 @@ public class Cat {
         return 0;
     }
 
-    public static int initStaticCount() {
+    public int initStaticCount() {
         System.out.println(5);
         return 0;
     }
 
     public Cat(String name) {
         this(0, name);
-        System.out.println(6);
+
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
+        System.out.println(6);
         System.out.println(7);
     }
 }
