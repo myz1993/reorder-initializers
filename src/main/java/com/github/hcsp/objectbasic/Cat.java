46038329e95f94ce332240cc9c4f1e9031a6f68b
mattisonZhao <mattison@yeah.net>
2019-10-15 09:43:51 +0800

    recorder initializers

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -3,39 +3,20 @@ package com.github.hcsp.objectbasic;
 public class Cat {
     public static int count = initStaticCount();
 
-    static {
-        System.out.println(1);
-    }
+    static {  System.out.println(2);}
 
     int age = initAge();
+
     String name;
 
-    {
-        System.out.println(2);
-    }
+    {System.out.println(4); }
+    { System.out.println(5) ;}
 
-    {
-        System.out.println(3);
-    }
+    public int initAge() { System.out.println(3); return 0; }
 
-    public int initAge() {
-        System.out.println(4);
-        return 0;
-    }
+    public static int initStaticCount() {System.out.println(1); return 0; }
 
-    public static int initStaticCount() {
-        System.out.println(5);
-        return 0;
-    }
+    public Cat(String name) {  this(0, name);  System.out.println(7) ;}
 
-    public Cat(String name) {
-        this(0, name);
-        System.out.println(6);
-    }
-
-    public Cat(int age, String name) {
-        this.age = age;
-        this.name = name;
-        System.out.println(7);
-    }
+    public Cat(int age, String name) { this.age = age; this.name = name; System.out.println(6); }
 }
