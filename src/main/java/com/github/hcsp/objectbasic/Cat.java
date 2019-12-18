d5d7a0ebbe15b279abb6ca46da6a4b23673c127b
astak16 <1500846601@qq.com>
2019-12-18 10:22:47 +0800

    对象初始化顺序 (#99)
    对象初始化顺序 (#99)

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,15 +1,9 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
-
     static {
         System.out.println(1);
     }
-
-    int age = initAge();
-    String name;
-
     {
         System.out.println(2);
     }
@@ -17,6 +11,10 @@ public class Cat {
     {
         System.out.println(3);
     }
+    int age = initAge();
+    String name;
+
+     int count = initStaticCount();
 
     public int initAge() {
         System.out.println(4);
@@ -30,12 +28,12 @@ public class Cat {
 
     public Cat(String name) {
         this(0, name);
-        System.out.println(6);
+        System.out.println(7);
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        System.out.println(7);
+        System.out.println(6);
     }
 }
