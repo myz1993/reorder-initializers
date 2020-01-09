dcb6e32b24ea45779cd0687f0cf8fdba78f9595e
fiymi <fiymi666@gmail.com>
2020-01-09 09:36:31 +0800

    调整初始化顺序

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -4,40 +4,42 @@ public class Cat {
 
 
     static {
-        System.out.println(1);
+        System.out.print(1+" ");
     }
 
     //int age = initAge();
      String name;
 
     {
-        System.out.println(2);
+        System.out.print(2+" ");
     }
 
     {
-        System.out.println(3);
+        System.out.print(3+" ");
     }
 
     int age = initAge();
 
     public int initAge() {
-        System.out.println(4);
+        System.out.print(4+" ");
         return 0;
     }
     int count = initStaticCount();
     public static int initStaticCount() {
-        System.out.println(5);
+        System.out.print(5+" ");
         return 0;
     }
 
     public Cat(String name) {
+
         this(0, name);
-        System.out.println(6);
+        System.out.print(7);
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        System.out.println(7);
+        System.out.print(6+" ");
+
     }
 }
