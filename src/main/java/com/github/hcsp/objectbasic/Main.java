f14a4b9b3616eaa869f4822ffb31d167d6492772
walkerxin <1464193658@qq.com>
2020-02-17 01:30:43 +0800

    remove redundant line

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,6 @@
 package com.github.hcsp.objectbasic;
 
 public class Main {
-    static int a = 2;
     public static void main(String[] args) {
         // 现在运行这个方法输出的数字是乱序的
         // 请将Cat中的初始化语句重新排序
