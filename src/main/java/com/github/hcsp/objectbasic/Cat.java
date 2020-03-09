d0fc56998c2aec8a986428a44ee883a6831e3ae9
tprox <61723644+tprox@users.noreply.github.com>
2020-03-09 16:14:12 +0800

    Fix print order (#144)
    Fix print order (#144)

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -4,13 +4,6 @@ public class Cat {
     public static int count = initStaticCount();
 
     static {
-        System.out.println(1);
-    }
-
-    int age = initAge();
-    String name;
-
-    {
         System.out.println(2);
     }
 
@@ -18,24 +11,32 @@ public class Cat {
         System.out.println(3);
     }
 
-    public int initAge() {
+    {
         System.out.println(4);
+    }
+
+    public int initAge() {
+        System.out.println(5);
         return 0;
     }
 
+    String name;
+    int age = initAge();
+
+
     public static int initStaticCount() {
-        System.out.println(5);
+        System.out.println(1);
         return 0;
     }
 
     public Cat(String name) {
         this(0, name);
-        System.out.println(6);
+        System.out.println(7);
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        System.out.println(7);
+        System.out.println(6);
     }
 }
