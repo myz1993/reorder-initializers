18439bc379d1715908176555b2081903be366f7b
wuzhenquan <782754354@qq.com>
2020-03-18 17:57:58 +0800

    对象的初始化顺序 (#148)
    对象的初始化顺序 (#148)

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,13 +1,10 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
-
     static {
         System.out.println(1);
     }
 
-    int age = initAge();
     String name;
 
     {
@@ -17,6 +14,10 @@ public class Cat {
     {
         System.out.println(3);
     }
+
+    int age = initAge();
+
+    int count = initStaticCount();
 
     public int initAge() {
         System.out.println(4);
@@ -30,12 +31,12 @@ public class Cat {
 
     public Cat(String name) {
         this(0, name);
-        System.out.println(6);
+        System.out.println(7);
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        System.out.println(7);
+        System.out.println(6);
     }
 }
