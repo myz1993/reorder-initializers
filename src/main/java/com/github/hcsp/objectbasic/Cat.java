6a1a6534de7804de041ff5eeb6b3acfbace8b423
Ma Haoxiang <61976611+strivejojo@users.noreply.github.com>
2020-04-07 20:49:45 +0800

    Java面向对象初步练习：对象的初始化顺序 (#159)
    Java面向对象初步练习：对象的初始化顺序 (#159)

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -3,39 +3,41 @@ package com.github.hcsp.objectbasic;
 public class Cat {
     public static int count = initStaticCount();
 
-    static {
+    public static int initStaticCount() {
         System.out.println(1);
+        return 0;
+    }
+
+    static {
+        System.out.println(2);
     }
 
     int age = initAge();
     String name;
 
-    {
-        System.out.println(2);
-    }
-
-    {
-        System.out.println(3);
-    }
-
     public int initAge() {
+        System.out.println(3);
+        return 0;
+    }
+
+    {
         System.out.println(4);
-        return 0;
     }
 
-    public static int initStaticCount() {
+    {
         System.out.println(5);
-        return 0;
-    }
-
-    public Cat(String name) {
-        this(0, name);
-        System.out.println(6);
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
+        System.out.println(6);
+    }
+
+    public Cat(String name) {
+        this(0, name);
         System.out.println(7);
     }
+
+
 }
