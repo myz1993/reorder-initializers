7bf00239fa8de52467ebf39de5377321d0fc5c4b
yyzcl <yyzclyang@gmail.com>
2020-06-10 23:57:30 +0800

    finish task (#181)
    finish task (#181)

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,13 +1,10 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
-
     static {
         System.out.println(1);
     }
 
-    int age = initAge();
     String name;
 
     {
@@ -18,24 +15,28 @@ public class Cat {
         System.out.println(3);
     }
 
+    int age = initAge();
+
     public int initAge() {
         System.out.println(4);
         return 0;
     }
 
-    public static int initStaticCount() {
+    public int count = initStaticCount();
+
+    public int initStaticCount() {
         System.out.println(5);
         return 0;
     }
 
     public Cat(String name) {
         this(0, name);
-        System.out.println(6);
+        System.out.println(7);
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        System.out.println(7);
+        System.out.println(6);
     }
 }
